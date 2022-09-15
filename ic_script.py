2ae4c7ec8a71cd069766a25ba9a905af576397b5
Matteo Frigo <matt.frigo@gmail.com>
2022-09-16 00:01:14 +0200

    change tracing dir

--- ic_script.py
+++ ic_script.py
@@ -1,4 +1,4 @@
 from src.main import createIC, multipleICcreator
 
-createIC(40, 'parent3', halolistfolder='./parent3', zoominlevel=12)
+createIC(40, 'parent3', halolistfolder='/ptmp/mpa/mfrigo/musicsims/parent3/halotracing', zoominlevel=12)
 #multipleICcreator('parent3', halolistfolder='./parent3', zoominlevel=12, startingatnumber=40)
