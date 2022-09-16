646079caf9e7d407b63371fea8059eadb7d209a4
Matteo Frigo <matt.frigo@gmail.com>
2022-09-16 09:09:23 +0200

    add workdir parameter

--- ic_script.py
+++ ic_script.py
@@ -1,7 +1,10 @@
 from src.main import createIC, multipleICcreator
 from src.rockstartools import rockstarselect
+from src.read_config import getproperties
 
+parentlabel="parent3"
 #rockstarselect('/ptmp/mpa/mfrigo/musicsims/parent3/Rockstar/trees/tree_0_0_0.dat', mvirmin=10e11, mvirmax=10e12, mindist=10.)
-createIC(40, 'parent3', halolistfolder='/ptmp/mpa/mfrigo/musicsims/parent3/halotracing', zoominlevel=12)
-#multipleICcreator('parent3', halolistfolder='./parent3', zoominlevel=12, startingatnumber=40)
+poslistdir, snapfilebase, ICfile, cosmology, boxsize, zstart, seedsset, parentres, lowestres = getproperties(parentlabel)
+createIC(40, parentlabel, halolistfolder=poslistdir+'/halotracing', zoominlevel=12)
+#multipleICcreator(parentlabel, halolistfolder='./parent3', zoominlevel=12, startingatnumber=40)
 
--- src/read_config.py
+++ src/read_config.py
@@ -7,7 +7,7 @@ def getproperties(parentlabel):
     Function that reads the properties of the parent simulations in use from config.yaml.
     For a given parent label, it returns:
 
-     poslistdir   : Directory of the position list files.
+     workdir      : Directory of the position list files and where the output ICs are saved.
      parent_snaps : Name/path of the snapshot files (without the number). If you have multiple subsnapshots, put it
                          in the form ["snapshot_dir","/snapshot_name"] (both without the number).
      parent_ic    : Name/path of the initial condition file of the parent simulation.
@@ -26,7 +26,7 @@ def getproperties(parentlabel):
     for section in cfg["parent_setups"]:
         if parentlabel == section:
             config_dict = cfg["parent_setups"][section]
-            return config_dict["poslistdir"], config_dict["parent_snaps"], config_dict["parent_ic"], \
+            return config_dict["workdir"], config_dict["parent_snaps"], config_dict["parent_ic"], \
                    config_dict["cosmology"], config_dict["boxsize"], config_dict["zstart"], config_dict["seedsset"], \
                    config_dict["parentres"], config_dict["lowestres"]
     raise IOError("parentlabel " + str(parentlabel) + " does not match any label in config.yaml")
